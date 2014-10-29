36f8819343dc856ffe43131cc1f3488eb1482c1c
caseykuhns <casey.kuhns@sparkfun.com>
2014-10-29 11:38:04 -0600

    added files to libs

---
+++ Libraries/Arduino/SFE_VL6180X.h
@@ -0,0 +1,42 @@
+/******************************************************************************
+SFE_VL6180X.h
+Library for VL6180x time of flight range finder.
+Casey Kuhns @ SparkFun Electronics
+10/29/2014
+https://github.com/sparkfun/MS5803-14BA_Breakout
+
+The VL6180x by ST micro is a time of flight range finder that
+uses pulsed IR light to determine distances from object at close
+range.  The average range of a sensor is between 0-200mm
+
+In this file are the function prototypes in the VL6180x class
+
+Resources:
+This library uses the Arduino Wire.h to complete I2C transactions.
+
+Development environment specifics:
+	IDE: Arduino 1.0.5
+	Hardware Platform: Arduino Pro 3.3V/8MHz
+	VL6180x Breakout Version: 1.0
+
+
+This code is beerware. If you see me (or any other SparkFun employee) at the
+local pub, and you've found our code helpful, please buy us a round!
+
+Distributed as-is; no warranty is given.
+******************************************************************************/
+
+#ifndef SFE_VL6180X_h
+#define SFE_VL6180X_h
+
+#include <Arduino.h>
+
+class VL6180x
+{
+	public:	
+	
+	private:
+	
+};
+
+#endif
