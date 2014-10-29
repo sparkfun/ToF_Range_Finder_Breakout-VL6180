0735c6923e784caba4aca2bbbd217785e71d6fa1
caseykuhns <casey.kuhns@sparkfun.com>
2014-10-29 11:37:43 -0600

    added files to libs

--- Libraries/Arduino/SFE_VL6180X.cpp
+++ Libraries/Arduino/SFE_VL6180X.cpp
@@ -1,15 +1,15 @@
 /******************************************************************************
-MS5803_I2C.cpp
-Library for MS5803 pressure sensor.
+SFE_VL6180x.cpp
+Library for VL6180x time of flight range finder.
 Casey Kuhns @ SparkFun Electronics
-6/26/2014
+10/29/2014
 https://github.com/sparkfun/MS5803-14BA_Breakout
 
-The MS58XX MS57XX and MS56XX by Measurement Specialties is a low cost I2C pressure
-sensor.  This sensor can be used in weather stations and for altitude
-estimations. It can also be used underwater for water depth measurements. 
+The VL6180x by ST micro is a time of flight range finder that
+uses pulsed IR light to determine distances from object at close
+range.  The average range of a sensor is between 0-200mm
 
-In this file are the functions in the MS5803 class
+In this file are the functions in the VL6180x class
 
 Resources:
 This library uses the Arduino Wire.h to complete I2C transactions.
@@ -17,7 +17,7 @@ This library uses the Arduino Wire.h to complete I2C transactions.
 Development environment specifics:
 	IDE: Arduino 1.0.5
 	Hardware Platform: Arduino Pro 3.3V/8MHz
-	MS5803 Breakout Version: 1.0
+	VL6180x Breakout Version: 1.0
 
 
 This code is beerware. If you see me (or any other SparkFun employee) at the
@@ -27,6 +27,22 @@ Distributed as-is; no warranty is given.
 ******************************************************************************/
 
 #include <Wire.h>
+#include "SFE_VL6180X.h"
+
+VL6180x::VL6180x(VL6180x_addr address)
+// Initialize the Library
+{
+	Wire.begin(); // Arduino Wire library initializer
+	address = address; //set interface used for communication
+}
+
+void VL6180x::reset(void)
+// Reset device
+{
+	//TODO: RESET!
+}
+
+
 
 #define CONT 8
 
